88538da4052fcc18be59e5254a4ec2e5f1e7ae11
Amanda Corrêa <amandacorreades@hotmail.com>
2021-09-05 18:02:57 -0300

    Atualização do Security

---
+++ Spring/blogpessoal/src/main/java/br/org/generation/blogpessoal/controller/UsuarioController.java
@@ -0,0 +1,83 @@
+package br.org.generation.blogpessoal.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.org.generation.blogpessoal.model.Usuario;
+import br.org.generation.blogpessoal.model.UsuarioLogin;
+import br.org.generation.blogpessoal.repository.UsuarioRepository;
+import br.org.generation.blogpessoal.service.UsuarioService;
+
+@RestController
+@RequestMapping("/usuario")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class UsuarioController {
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+
+	@Autowired
+	private UsuarioService usuarioService;
+
+	@GetMapping("/all")
+	public ResponseEntity<List<Usuario>> getAll() {
+
+		return ResponseEntity.ok(usuarioRepository.findAll());
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> getById(@PathVariable long id) {
+		return usuarioRepository.findById(id).map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
+
+	}
+
+	@GetMapping("/nome/{nome}")
+	public ResponseEntity<List<Usuario>> getByNome(@PathVariable String nome) {
+
+		return ResponseEntity.ok(usuarioRepository.findAllByNomeContainingIgnoreCase(nome));
+
+	}
+
+	@PostMapping("/cadastrar")
+	public ResponseEntity <Usuario> Post(@RequestBody Usuario usuario) {
+
+		Usuario usuarioResp = usuarioService.cadastrarUsuario(usuario);
+		try {
+			return ResponseEntity.status(HttpStatus.CREATED).body(usuarioResp);
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().build();
+		}
+
+	}
+
+	@PostMapping("/logar")
+	public ResponseEntity<UsuarioLogin> autenticationUsuario(@RequestBody Optional<UsuarioLogin> usuario) {
+		return usuarioService.logarUsuario(usuario).map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+	}
+
+	@PutMapping("/alterar")
+	public ResponseEntity<Usuario> putUsuario(@RequestBody Usuario usuario) {
+		Optional<Usuario> updateUsuario = usuarioService.atualizarUsuario(usuario);
+
+		try {
+			return ResponseEntity.ok(updateUsuario.get());
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().build();
+		}
+	}
+
+}
